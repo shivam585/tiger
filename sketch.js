a1a6d93c0c5029bb5e3fadef3b0c52b2d21ccc17
shivam585 <85876404+shivam585@users.noreply.github.com>
2021-06-21 17:02:43 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -43,7 +43,9 @@ backimage=loadImage("forestback.jpg")
 function setup() {
   createCanvas(windowWidth,windowHeight);
   
- 
+ back=createSprite(width/2,height/2,1000000,100000);
+back.addImage(backimage)
+back.scale =50;
   
   trex = createSprite(50,180,20,50);
   trex.addAnimation("running", trex_running);
